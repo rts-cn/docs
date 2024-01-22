8b9c2e3661c7e14b13dd95195bd4778179495784
Seven Du <dujinfang@x-y-t.cn>
2024-01-22 11:24:15 +0800

    use lower case

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/RTS/"
+            to="/rts/"
           >
             开始阅读
           </Link>
